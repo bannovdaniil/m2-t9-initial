c55092f60e037e38e1bcfb7963657fbc9a7cf519
bannovdaniil <baddan@mail.ru>
2022-06-15 18:05:21 +0300

    @ResponseStatus,

--- src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
+++ src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.exception.IncorrectCountException;
 
@@ -35,8 +36,31 @@ public class CatsInteractionController {
     }
 
     @ExceptionHandler
-    public Map<String, String> handleIncorrectCount(final IncorrectCountException err) {
-        return Map.of("error", "Ошибка с параметром count.",
-                "error message", err.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHappinessOverflow(final IncorrectCountException err) {
+        return new ErrorResponse(
+                "Ошибка с параметром count.", err.getMessage()
+        );
+    }
+
+    public class ErrorResponse {
+        // название ошибки
+        String error;
+        // подробное описание
+        String description;
+
+        public ErrorResponse(String error, String description) {
+            this.error = error;
+            this.description = description;
+        }
+
+        // геттеры необходимы, чтобы Spring Boot мог получить значения полей
+        public String getError() {
+            return error;
+        }
+
+        public String getDescription() {
+            return description;
+        }
     }
 }
